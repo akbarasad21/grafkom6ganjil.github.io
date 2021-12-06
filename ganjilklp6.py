5e0fe54ec73afb50c6f8b76ae72dff1857148c2c
akbarasad21 <77686207+akbarasad21@users.noreply.github.com>
2021-12-06 19:17:52 +0800

    oleh akbar
    membuat inputan

---
+++ ganjilklp6.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = int(input('Masukan x1  : '))
+y1 = int(input('Masukan y1  : '))
+x2 = int(input('Masukan x2  : '))
+y2 = int(input('Masukan y2  : '))
